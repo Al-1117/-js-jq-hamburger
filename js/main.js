be9bd68a5cb0c1cfe5b44d401605727d044852be
Alioune <alioune1117@gmail.com>
2020-06-08 15:55:09 +0200

    provato a nasconderlo una volta scoperto, ma per ora non funziona

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 
+
+// COMPARSA DEL HAMBURGER MENU
+var hamburger = false;
+
 // Seleziono il collegamento all'hamburger menu e gli aggiungo il click
 $('.header-right .fa-bars').click(
-    // Aggiungo la classe active già presente nel CSS per mostrarlo
   function(){
-  $('.hamburger-menu').addClass('active');
-}
+    if (hamburger == false) {
+      // Aggiungo la classe active già presente nel CSS per mostrarlo
+      $('.hamburger-menu').addClass('active');
+
+    hamburger = true;
+    }
+
+    // SCOMPARSA DELL'HAMBURGER MENU
+    else if (hamburger == true) {
+      // Aggiungo la funzione per nasconderlo
+      $('.hamburger-menu').hide();
+
+      hamburger = false;
+    }
+
+  }
+
 );
+
+// SCONPARSA DELL'HAMBURGE MENU
