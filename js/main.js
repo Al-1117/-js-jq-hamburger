02448cfa6305b93c3ace7932606d01d01a45fb43
Alioune <alioune1117@gmail.com>
2020-06-08 15:17:29 +0200

    caricato i files dell'esercizio.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+// Seleziono il collegamento all'hamburger menu e gli aggiungo il click
+$('.header-right .fa-bars').click(
+    // Aggiungo la classe active già presente nel CSS per mostrarlo
+  function(){
+  $('.hamburger-menu').addClass('active');
+}
+);
